e3961da58e95a59ddde5f0a3060e175b8bcaa4eb
danielisgr8 <1stfretgamer@gmail.com>
2016-01-31 23:14:42 -0600

    added x slingshot aim, angle change doesn't work at all

--- slingshot/main.js
+++ slingshot/main.js
@@ -1,100 +1,78 @@
-// var interval, friction, frictionInterval = false;
+function pi(variable) {
+	if(isNaN(parseInt(variable, 10))) {
+		return 0;
+	} else {
+		return parseInt(variable, 10);
+	}
+}
+function style(element, property) {
+	return window.getComputedStyle(element).getPropertyValue(property);
+}
 var div = document.getElementById("drag");
 div.draggable = true;
-div.style.left = 0;
-div.style.top = 0;
+var line, lineHolder = document.getElementById("lineHolder");
+line = document.createElement("div");
+line.id = "line";
+line.style.width = "20px";
+line.style.height = "20px";
+div.style.width = "100px";
+div.style.height = "100px";
+div.style.left = "500px";
+div.style.top = "500px";
+var xBefore, xAfter, xOrigin, yBefore, yAfter, yOrigin;
 var img = document.createElement("img");
 img.src = "https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png";
-var xBefore, xAfter, yBefore, yAfter;
-// var xVelocity, vxBefore, vxAfter, yVelocity, vyBefore, vyAfter;
 div.ondragstart = function(e) {
 	e.dataTransfer.setDragImage(img, 0, 0);
 	div.style.backgroundColor = "red";
 	xBefore = e.screenX;
+	xOrigin = e.screenX;
 	yBefore = e.screenY;
-	// vxBefore = e.screenX;
-	// vyBefore = e.screenY;
-	// interval = window.setInterval(function() {
-	// 	if(typeof xAfter != "undefined" && typeof yAfter != "udnefined") {
-	// 		friction();
-	// 	}
-	// }, 5);
-	// function friction() {
-	// 	xVelocity = vxAfter - vxBefore;
-	// 	if(xVelocity > 75) {
-	// 		xVelocity = 75;
-	// 	} else if(xVelocity < -75) {
-	// 		xVelocity = -75;
-	// 	}
-	// 	vxBefore = vxAfter;
-	// 	yVelocity = vyAfter - vyBefore;
-	// 	if(yVelocity > 75) {
-	// 		yVelocity = 75;
-	// 	} else if(yVelocity < -75) {
-	// 		yVelocity = -75;
-	// 	}
-	// 	vyBefore = vyAfter;
-	// }
-	// if(frictionInterval) {
-	// 	window.clearInterval(frictionInterval);
-	// 	frictionInterval = false;
-	// }
+	yOrigin = e.screenY;
+	lineHolder.style.left = div.style.left;
+	lineHolder.style.top = div.style.top;
+	line.style.display = "none";
+	lineHolder.appendChild(line);
 }
 div.ondrag = function(e) {
+	line.style.display = "block";
 	xAfter = e.screenX;
 	yAfter = e.screenY;
-	// vxAfter = e.screenX;
-	// vyAfter = e.screenY;
 	if(xBefore != xAfter && xAfter != 0) {
 		var xDif = xAfter - xBefore;
-		if(parseInt(div.style.left, 10) + xDif < 0) {
-			div.style.left = "0px";
-		} else if(parseInt(div.style.left, 10) + xDif > document.documentElement.clientWidth - 100) {
-			div.style.left = document.documentElement.clientWidth - 100 + "px";
-		} else {
-			div.style.left = parseInt(div.style.left, 10) + xDif + "px";
+		if(xAfter < xOrigin) {
+			// width + left = 60
+			if(pi(style(line, "width")) + pi(style(line, "left")) != 60) {
+				line.style.width = "20px";
+				line.style.left = "40px";
+			} else {
+			line.style.width = pi(style(line, "width")) - xDif + "px";
+			line.style.left = pi(style(line, "left")) + xDif + "px";
+			}
+		} else if(xAfter > xOrigin) {
+			line.style.left = "40px";
+			line.style.width = pi(style(line, "width")) + xDif + "px";
+		} else if(xAfter == xOrigin) {
+			line.style.width = "20px";
+			line.style.left = "40px";
 		}
 		xBefore = xAfter;
 	}
+	// height should not change, angle should
 	if(yBefore != yAfter && yAfter != 0) {
 		var yDif = yAfter - yBefore;
-		if(parseInt(div.style.top, 10) + yDif < 0) {
-			div.style.top = "0px";
-		} else if(parseInt(div.style.top, 10) + yDif > document.documentElement.clientHeight - 100) {
-			div.style.top = document.documentElement.clientHeight - 100 + "px";
-		} else {
-			div.style.top = parseInt(div.style.top, 10) + yDif + "px";
-		}
 		yBefore = yAfter;
 	}
+	if(typeof yDif != "undefined" && typeof xDif != "undefined") {
+		var angle = Math.atan(yDif / xDif);
+		line.style.transform = "rotate(" + pi(style(line, "transform")) + angle + "rad)"; // almost always 45 degrees
+	}
+
 }
 div.ondragend = function(e) {
 	div.style.backgroundColor = "blue";
-	// window.clearInterval(interval);
-	// frictionInterval = window.setInterval(function() {
-	// 	if(parseInt(div.style.left, 10) + xVelocity < 0) {
-	// 		div.style.left = "0px";
-	// 		xVelocity = -xVelocity;
-	// 	} else if(parseInt(div.style.left, 10) + xVelocity > document.documentElement.clientWidth - 100) {
-	// 		div.style.left = document.documentElement.clientWidth - 100 + "px";
-	// 		xVelocity = -xVelocity;
-	// 	}
-	// 	div.style.left = parseInt(div.style.left, 10) + xVelocity + "px";
-	// 	if(parseInt(div.style.top, 10) + yVelocity < 0) {
-	// 		div.style.top = "0px";
-	// 		yVelocity = -yVelocity;
-	// 	} else if(parseInt(div.style.top, 10) + yVelocity > document.documentElement.clientHeight - 100) {
-	// 		div.style.top = document.documentElement.clientHeight - 100 + "px";
-	// 		yVelocity = -yVelocity;
-	// 	}
-	// 	div.style.top = parseInt(div.style.top, 10) + yVelocity + "px";
-	// }, 5);
+	// lineHolder.removeChild(line);
+	line.style.width = "20px";
+	line.style.left = "40px";
+	// line.style.transform = "rotate(0rad)";
 }
-// document.onkeypress = function(e) {
-// 	if(e.charCode == 32) {
-// 		xVelocity = 0;
-// 		yVelocity = 0;
-// 		window.clearInterval(frictionInterval);
-// 		frictionInterval = false;
-// 	}
-// }
