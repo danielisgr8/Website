43439ee642e14eccb16c6e4a0a6da4c2c9d017cf
danielisgr8 <1stfretgamer@gmail.com>
2016-04-17 23:08:38 -0500

    buncha stuff

--- minesweeper/main.js
+++ minesweeper/main.js
@@ -1,10 +1,14 @@
 var pre = document.getElementsByTagName("pre")[0];
 pre.innerHTML = "";
 var Game = {};
-Game.width = 10;
-Game.height = 10;
-Game.map = [[]];
+Game.state = 0; // 0 = not generated, not started, 1 = generated, not started, 2 = generated, started
+Game.width;
+Game.height;
+var regex = /x\d+y\d+/;
+var regexX = /x\d+/;
+var regexY = /y\d+/;
 Game.createBoard = function() {
+	Game.state = 1;
 	var add = "";
 	for(var i = 0; i < Game.width; i++) {
 		add += " ___";
@@ -23,7 +27,83 @@ Game.createBoard = function() {
 	pre.innerHTML += add;
 }
 Game.start = function() {
-
+	if(Game.state == 1) {
+		Game.state = 2;
+		for(var i = 0; i < Game.width; i++) {
+			for(var j = 0; j < Game.height; j++) {
+				var random = Math.random();
+				var element = document.getElementsByClassName("x" + i + "y" + j);
+				if(random < .5) {
+					element[0].className += " m";
+					element[0].innerHTML = " m ";
+					element[1].className += " m";
+				} else {
+					element[0].className += " b";
+					element[0].innerHTML = " b ";
+					element[1].className += " b";
+				}
+			}
+		}
+	}
+}
+Game.getArea = function(start) {
+	if(Game.state == 2) {
+		var searching = true;
+		var type;
+		if(start.className.includes("m")) {
+			type = "m";
+		} else {
+			type = "b";
+		}
+		var startX = parseInt(regexX.exec(start.className)[0].slice(1), 10);
+		var startY = parseInt(regexY.exec(start.className)[0].slice(1), 10);
+		var start = document.getElementsByClassName("x" + startX + "y" + startY)[0];
+		var area = [start];
+		var outer = [start];
+		while(searching) {
+			var length = outer.length;
+			for(var foo = 0; foo < length; foo++) {
+				var x = parseInt(regexX.exec(outer[foo].className)[0].slice(1), 10);
+				var y = parseInt(regexY.exec(outer[foo].className)[0].slice(1), 10);
+				var coordsX = [-1, 1, 0, 0];
+				var coordsY = [0, 0, -1, 1];
+				for(var i = 0; i < coordsX.length; i++) {
+					if(x + coordsX[i] >= 0 && x + coordsX[i] < Game.width && y + coordsY[i] >= 0 && y + coordsY[i] < Game.height) {
+						var element = document.getElementsByClassName("x" + (x + coordsX[i]) + "y"  + (y + coordsY[i]))[0];
+						if(element.className.includes(type) && !area.includes(element)) {
+							area.push(element);
+							outer.push(element);
+						}
+					}
+				}
+			}
+			if(outer.length == length) {
+				searching = false;
+			} else {
+				outer.slice(length);
+			}
+		}
+		return area.length;
+	}
+}
+Game.getMineCount = function(element) {
+	if(Game.state == 2 && element.className.includes("b")) {
+		var mineCount = 0;
+		var x = parseInt(regexX.exec(element.className)[0].slice(1), 10);
+		var y = parseInt(regexY.exec(element.className)[0].slice(1), 10);
+		for(var i = -1; i <= 1; i++) {
+			for(var j = -1; j <= 1; j++) {
+				if(x + i >= 0 && x + i < Game.width && y + j >= 0 && y + j < Game.height) {
+					if(document.getElementsByClassName("x" + (x + i) + "y"  + (y + j))[0].className.includes("m")) {
+						mineCount++;
+					}
+				}
+			}
+		}
+		return mineCount;
+	} else {
+		return "m";
+	}
 }
 var heightInput = document.getElementsByTagName("input")[0];
 var widthInput = document.getElementsByTagName("input")[1];
@@ -35,19 +115,20 @@ submit.onclick = function(e) {
 	pre.innerHTML = "";
 	Game.createBoard();
 }
+start.onclick = function(e) {
+	Game.start();
+}
 pre.onclick = function(e) {
 	if(e.target !== e.currentTarget) {
-		var clickedItem = e.target.className;
-		document.getElementsByClassName(clickedItem)[0].style.backgroundColor = "red";
-		document.getElementsByClassName(clickedItem)[1].style.backgroundColor = "red";
+		var clickedItem = e.target;
+		document.getElementsByClassName(clickedItem.className)[0].innerHTML = " " + Game.getMineCount(clickedItem) + " ";
 	}
 	e.stopPropagation();
 }
 pre.oncontextmenu = function(e) {
 	if(e.target !== e.currentTarget) {
-		var clickedItem = e.target.className;
-		document.getElementsByClassName(clickedItem)[0].style.backgroundColor = "blue";
-		document.getElementsByClassName(clickedItem)[1].style.backgroundColor = "blue";
+		var clickedItem = e.target;
+		document.getElementsByClassName(clickedItem.className)[0].innerHTML = " F ";
 	}
 	e.stopPropagation();
 	return false;
